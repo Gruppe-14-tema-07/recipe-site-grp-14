48b660d12a53120d9e77f62f688aa0bc6c9269dc
solom0001 <somw0001@stud.kea.dk>
2025-02-19 16:10:53 +0100

    index 25min section plus header update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+const queryString = window.location.search;
+const searchParams = new URLSearchParams(queryString);
+const productId = searchParams.get("id");
+
 const container = document.querySelector(".container");
 let mealArray = [];
 let cuisineArray = [];
@@ -33,3 +37,33 @@ fetch(`https://dummyjson.com/recipes`)
     console.log("Unique meal types:", mealArray);
     console.log("Unique cuisines:", cuisineArray);
   });
+
+const minutescontainer = document.getElementById("box1-2");
+fetch("https://dummyjson.com/recipes") //fetch daten som bliver filtrered til et nyt array og kalder
+  .then((response) => response.json()) //sikre at responsen er i json format
+  .then((data) => {
+    return data.recipes.filter((recipe) => recipe.prepTimeMinutes + recipe.cookTimeMinutes <= 25); //brug filter()at loop igennem recipes og gem dem under 25 min in Arrayet
+  })
+  .then((quickRecipes) => {
+    showProduct(quickRecipes);
+  });
+
+function showProduct(quickRecipes) {
+  const markup = quickRecipes
+    .map(
+      (recipe) => `
+                    
+                         <a href="product.html?id=${recipe.id}">
+                        <div>
+                            <p>${recipe.name}</p>
+
+                            <p class="pClass">&#x2197;</p>
+                        </div>
+                        </a>
+                        
+ `
+    )
+    .join(" ");
+
+  minutescontainer.innerHTML = markup;
+}
