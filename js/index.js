96d9a31da94d07c51e6bd7c9a3ac57ed223f7b02
solom0001 <somw0001@stud.kea.dk>
2025-02-20 09:49:33 +0100

    index

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@ const queryString = window.location.search;
 const searchParams = new URLSearchParams(queryString);
 const productId = searchParams.get("id");
 
-const container = document.querySelector(".container");
+const mealTypeContainer = document.querySelector(".mealType");
+const cuisineContainer = document.querySelector(".cuisine");
+const mealType = new URLSearchParams(window.location.search).get("mealType");
+
+const container = document.querySelector(".Mycontainer");
 let mealArray = [];
 let cuisineArray = [];
 
@@ -66,4 +70,73 @@ function showProduct(quickRecipes) {
     .join(" ");
 
   minutescontainer.innerHTML = markup;
+}
+
+// Fetch all products (to get meal types)
+fetch("https://dummyjson.com/recipes")
+  .then((response) => response.json())
+  .then((products) => {
+    products.recipes.forEach((data) => {
+      if (Array.isArray(data.mealType)) {
+        data.mealType.forEach((type) => {
+          if (!mealArray.includes(type) && type !== "Snack") {
+            mealArray.push(type);
+          }
+        });
+      }
+    });
+
+    // Display meal types
+    mealTypeContainer.innerHTML = mealArray
+      .map((data) => {
+        return `
+          <a href="productlist.html?mealType=${data}">
+            <div class="flexcol">
+              <img src="img/${data}.png" alt="${data}">
+              <p>${data}</p>
+            </div>
+          </a>`;
+      })
+      .join(" ");
+  });
+
+fetch("https://dummyjson.com/recipes?limit=4")
+  .then((response) => response.json())
+  .then((data) => {
+    return data.recipes.sort((a, b) => b.reviewCount - a.reviewCount); // Sorter reviewCount fra Største til mindst
+  })
+  .then((topRecipes) => {
+    showProducts(topRecipes);
+    console.log("These are the top recipes: ", topRecipes);
+  });
+
+function showProducts(topRecipes) {
+  const markup = topRecipes
+    .map(
+      (data) => `
+    
+      <div class="recipeBox">
+                  <div class="recipeImg">
+                    <img class="recipeInnerImg" src="https://cdn.dummyjson.com/recipe-images/${data.id}.webp" alt="${data.name}">
+                  </div>
+                  <div class="info">
+                    <h2 class="time">${data.time} mins</h2>
+                    <h2 class="recipeBoxName">${data.name}</h2>
+                    <p class="tags">${data.tags ? data.tags.join(", ") : ""}</p>
+                    <div class="flexrow iconbox" style="justify-content: space-around;">
+                      <div class="flexrow">
+                        <img class="star" src="svg/Star.svg" alt="Rating star">
+                        <h2 class="rating">${data.rating}</h2>
+                      </div>
+                      <div class="flexrow">
+                        <img class="serving" src="svg/Servings.svg" alt="Servings icon">
+                        <h2 class="servingsText">${data.servings} Servings</h2>
+                      </div>
+                    </div>
+                  </div>
+                </div> `
+    )
+    .join(" ");
+
+  container.innerHTML = markup;
 }
